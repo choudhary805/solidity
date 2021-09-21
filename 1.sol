55ff53d103df2d6c1c17c3f4ab790857027693c3
abhishek choudhry <66086502+choudhary805@users.noreply.github.com>
2021-09-21 15:16:12 +0530

    Create 1.sol

---
+++ 1.sol
@@ -0,0 +1,15 @@
+pragma solidity >=0.4.0 <0.9.0;
+contract Abhi{
+    string private name;
+    uint private num;
+    constructor() public{
+        name="choudhary";
+        num=25;
+    }
+    function getname() view public returns(string memory){
+        return name;
+    }
+    function getnum() view public returns(uint){
+        return num;
+    }
+}
